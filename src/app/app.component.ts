f27a92e604bb31eaa0139211cb85469492111d7c
Frank Strieter <fs@co-it.eu>
2018-06-12 18:01:33 +0200

    prepares project for leipzig

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ngdd-root',
+  selector: 'ngle-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ngdd';
+  title = 'ngLE';
 }
